32e0bd2a23fa698b87b53755355ff45e789c8f3d
Víctor Cardozo Fernández <vcardozof@gmail.com>
2023-10-23 15:55:47 +0200

    Initial Commit

---
+++ src/main/java/com/vcardozof/oauthdemoapi/OauthDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.vcardozof.oauthdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthDemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcardozof/oauthdemoapi/OauthDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcardozof.oauthdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
